16dcefffb66148534ca2d6946d403e54559ac936
Adam Rogers <arogers0311@gmail.com>
2020-03-21 15:24:55 +1100

    initial

---
+++ Client/Program.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Text;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using blazorhostedgrpc32.Shared;
+
+using GrpcGreeter;
+using Grpc.Net.Client;
+using Grpc.Net.Client.Web;
+using Microsoft.AspNetCore.Components;
+using System.Net.Http;
+
+namespace blazorhostedgrpc32.Client
+{
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var builder = WebAssemblyHostBuilder.CreateDefault(args);
+            builder.RootComponents.Add<App>("app");
+
+            builder.Services.AddBaseAddressHttpClient();
+
+            builder.Services.AddSingleton(services =>
+            {
+                // Create a gRPC-Web channel pointing to the backend server
+                var httpClient = new HttpClient(new GrpcWebHandler(GrpcWebMode.GrpcWeb, new HttpClientHandler()));
+                var baseUri = services.GetRequiredService<NavigationManager>().BaseUri;
+                var channel = GrpcChannel.ForAddress(baseUri, new GrpcChannelOptions { HttpClient = httpClient });
+
+            // Now we can instantiate gRPC clients for this channel
+            return new Greeter.GreeterClient(channel);
+            });
+
+            await builder.Build().RunAsync();
+        }
+    }
+}
